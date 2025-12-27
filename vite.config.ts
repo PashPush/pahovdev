67c42da6ea8c9a23b458b3e46512f8f7886e24ec
Pavel Khovalkin <khovalkin@radyushin.com>
2025-12-27 22:32:37 +0400

    add homepage property

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import glsl from 'vite-plugin-glsl';
-import { visualizer } from 'rollup-plugin-visualizer';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), glsl(), visualizer({ open: true })],
+  plugins: [react(), tailwindcss(), glsl() /* , visualizer({ open: true }) */],
   build: {
     rollupOptions: {
       output: {
